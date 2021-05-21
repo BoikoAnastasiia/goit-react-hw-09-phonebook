efb1bef2df0d90a8ed562983ed8254a16fdf3cdc
Anastasiia Boiko <nastasyah86@gmail.com>
2021-05-21 14:01:30 +0300

    privite route

--- src/Components/Routes/PrivateRoute.jsx
+++ src/Components/Routes/PrivateRoute.jsx
@@ -1,5 +1,5 @@
 import { Route, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { authSelectors } from '../../redux/auth';
 
 /**
@@ -7,22 +7,16 @@ import { authSelectors } from '../../redux/auth';
  * Otherwise it renders Redirect to /login
  */
 
-const PrivateRoute = ({
-  component: Component,
+export default function PrivateRoute({
+  children,
   isAuthenticated,
   redirectTo,
   ...routeProps
-}) => (
-  <Route
-    {...routeProps}
-    render={props =>
-      isAuthenticated ? <Component {...props} /> : <Redirect to={redirectTo} />
-    }
-  />
-);
-
-const mapStateToProps = state => ({
-  isAuthenticated: authSelectors.getIsAuthenticated(state),
-});
-
-export default connect(mapStateToProps)(PrivateRoute);
+}) {
+  const isLoggedIn = useSelector(authSelectors.getIsAuthenticated);
+  return (
+    <Route {...routeProps}>
+      {isLoggedIn ? children : <Redirect to={redirectTo} />}
+    </Route>
+  );
+}
